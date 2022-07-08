73ed16b1936af0baac203e544a84b56c43453a04
Anton <nevero.anton@gmail.com>
2022-07-08 19:40:45 +0300

    task from codewars

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import random
 
 def main():
-    month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    names_of_months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october",
-                       "november", "december"]
-    with open("text.txt", "r") as file:
-        num_of_month = 1
-        for i in month:
-            days = 1
-            step = 0
-            while days <= i:
-                step += int(file.readline())
-                days += 1
-            print(f"Month {names_of_months[num_of_month-1]}: {round(step / i)}")
-            num_of_month += 1
+    print(get_count("antan"))
+
+def get_count(sentence):
+    result = 0
+    vowels = ["a", "e", "i", "o", "u"]
+    for i in vowels:
+        for j in sentence:
+            if j == i:
+                result += 1
+    return result
 
 main()
