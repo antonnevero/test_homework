eadb6c42cf1144cef32e70ddff8ce1060580addc
Anton <nevero.anton@gmail.com>
2022-07-08 19:42:28 +0300

    new version

--- main.py
+++ main.py
@@ -4,12 +4,6 @@ def main():
     print(get_count("antan"))
 
 def get_count(sentence):
-    result = 0
-    vowels = ["a", "e", "i", "o", "u"]
-    for i in vowels:
-        for j in sentence:
-            if j == i:
-                result += 1
-    return result
+    return sum(1 for let in sentence if let in "aeiouAEIOU")
 
 main()
