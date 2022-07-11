57bd69805c89a6c9e348cbf471f9aa679fe8cc6a
Anton <nevero.anton@gmail.com>
2022-07-11 20:10:47 +0300

    Print 2D list

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ ROWS = 3
 COLS = 4
 def main():
     list_2d = [[0 for r in range(COLS)] for c in range(ROWS)]
+    for r in range(ROWS):
+        for c in range(COLS):
+            print(list_2d[r][c])
 
-    print(list_2d)
+    # print(list_2d)
 
 
 
