619b53c8fd2e5b874a5181f6c460b8e8137c0df8
Anton <nevero.anton@gmail.com>
2022-07-11 20:12:58 +0300

    Print 2D list v2.0

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
-ROWS = 3
-COLS = 4
+ROWS = 4
+COLS = 2
 def main():
-    list_2d = [[0 for r in range(COLS)] for c in range(ROWS)]
+    list_2d = [[1, 2], [10, 20], [100, 200], [1000, 2000]]
     for r in range(ROWS):
         for c in range(COLS):
             print(list_2d[r][c])
