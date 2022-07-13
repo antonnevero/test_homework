2568795922e59f26bacb14a6a3d70bc242ad750a
Anton <nevero.anton@gmail.com>
2022-07-13 19:32:15 +0300

    Print 2D list v3.0

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import random
-ROWS = 4
-COLS = 2
-def main():
-    list_2d = [[1, 2], [10, 20], [100, 200], [1000, 2000]]
-    for r in range(ROWS):
-        for c in range(COLS):
-            print(list_2d[r][c])
 
-    # print(list_2d)
+def main():
+    list_2d = [[0 for i in range(3)] for i in range(5)]
+
+    for r in range(5):
+        for c in range(3):
+            list_2d[r][c] = int(input("Enter number: "))
+    print(list_2d)
 
 
 
