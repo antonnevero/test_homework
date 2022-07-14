d1ed7676fe3b42782319b12d81e225840ee83cc6
Anton <nevero.anton@gmail.com>
2022-07-14 20:02:27 +0300

    Right answers for exam

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import random
 
 def main():
-    numbers = [1, 2, 5, 8, 10]
-    num = 4
-    bigger_that_n(numbers, num)
-
-def bigger_that_n(list_of_numbers, n):
-    for l in list_of_numbers:
-        if l > n:
-            print(l)
-
-
+    right_answers = ['A', 'C', 'A', 'A', 'D', 'B', 'C', 'A', 'C', 'B', 'A', 'D', 'C', 'A', 'D', 'C', 'B', 'B', 'D', 'A']
+    student_answers = []
+    wrong_answers = []
+    with open("text.txt") as file:
+        for c in file:
+            student_answers.append(c.strip("\n"))
+    count = 0
+    count_not_right = 0
+    for i in range(len(right_answers)):
+        if right_answers[i] == student_answers[i]:
+            count += 1
+        else:
+            count_not_right += 1
+            wrong_answers.append(i + 1)
+    if count > 14:
+        print("You're good!")
+    else:
+        print("Fuck you!")
+    print(count)
+    print(count_not_right)
+    print(f"Wrong answers for questions {wrong_answers}")
 
 
 main()
