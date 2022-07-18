521699767b707f7492fd819b2c77c940ab3db908
Anton <nevero.anton@gmail.com>
2022-07-18 20:05:04 +0300

    Prime numbers

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import random
 
 def main():
-    lo_shu = [[4, 9, 2], [3, 5, 7], [8, 1, 6]]
-    print(magic(lo_shu))
+    answer = int(input("Enter number: "))
+    list_numbers = []
+    for i in range(2, answer+1):
+        list_numbers.append(i)
 
-def magic(magic_list):
-    first_row = magic_list[0][0] + magic_list[0][1] + magic_list[0][2]
-    second_row = magic_list[1][0] + magic_list[1][1] + magic_list[1][2]
-    third_row = magic_list[2][0] + magic_list[2][1] + magic_list[2][2]
-    first_col = magic_list[0][0] + magic_list[1][0] + magic_list[2][0]
-    second_col = magic_list[0][1] + magic_list[1][1] + magic_list[2][1]
-    third_col = magic_list[0][2] + magic_list[1][2] + magic_list[2][2]
-    first_diag = magic_list[0][2] + magic_list[1][1] + magic_list[2][0]
-    second_diag = magic_list[0][0] + magic_list[1][1] + magic_list[2][2]
+    for i in list_numbers:
+        simple_number(i)
+
+def simple_number(number):
+    flag = False
+    for i in range(2, number):
+        if number % i == 0:
+            flag = False
+            break
+        else:
+            flag = True
+    if flag:
+        print(number)
 
 
-    return first_row == second_row == third_row == first_col == second_col == third_col == first_diag == second_diag
 
 
 if __name__ == '__main__':
