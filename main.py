6a1ab9542500a3b5c59d3e1ee50c9c612f117e19
Anton <nevero.anton@gmail.com>
2022-07-19 20:14:20 +0300

    Magic Ball v1.1

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ import random
 
 def main():
     answers = []
-    try:
-        with open("text.txt", "r") as file:
-            for i in file:
-                answers.append(i.strip())
+
+    with open("text.txt", "r") as file:
+        for i in file:
+            answers.append(i.strip())
+    question = input("Write a question or E for exit: ")
+    while question != "E" and question != "e":
+        number = random.randint(0, 11)
+        print(answers[number])
         question = input("Write a question or E for exit: ")
-        while question != "E" or question != "e":
-            number = random.randint(0, 11)
-            print(answers[number])
-            question = input("Write a question or E for exit: ")
-    except FileNotFoundError:
-        print("Error")
+
 
 
 
