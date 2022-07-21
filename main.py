ba856a36b48d487b5162c6e13c2e047bcb44f312
Anton <nevero.anton@gmail.com>
2022-07-21 19:34:42 +0300

    Magic Ball v1.2

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ def main():
     with open("text.txt", "r") as file:
         for i in file:
             answers.append(i.strip())
-    question = input("Write a question or E for exit: ")
-    while question != "E" and question != "e":
+    question = input("Write a question or E for exit: ").lower()
+    while question != "e":
         number = random.randint(0, 11)
         print(answers[number])
-        question = input("Write a question or E for exit: ")
+        question = input("Write a question or E for exit: ").lower()
 
 
 
