6a58027040640b80eb41af125d297756b228eb83
Anton <nevero.anton@gmail.com>
2022-07-21 20:13:00 +0300

    Price of gas in 1994

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import random
 import matplotlib.pyplot as plt
-
+WEEKS = 52
 def main():
-    expenses = []
+    y_coor = []
     with open("text.txt") as file:
         for i in file:
-            expenses.append(int(i.strip()))
-    slices = ["Квартплата", "Бензин", "Еда", "Одежда", "Машина", "Разное"]
-    plt.pie(expenses, labels=slices)
+            y_coor.append(float(i.strip()))
+    x_coor = []
+    for i in range(WEEKS):
+        x_coor.append(i)
+    plt.plot(x_coor, y_coor, marker='o')
+    plt.title("Стоимость бензина за 1994 год")
+    plt.xlabel("Недели")
+    plt.ylabel("Стоимость")
+    plt.grid(True)
     plt.show()
+
 
 
 
