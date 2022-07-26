2abac08b46e71ee01ae598015dc9ede483dd5fc3
Anton <nevero.anton@gmail.com>
2022-07-26 19:37:08 +0300

    Words to Phone

--- main.py
+++ main.py
@@ -2,16 +2,33 @@ import random
 
 
 def main():
-    user_date = input("Your date: ")
-    list_u = user_date.split('/')
-    date = list_u[0]
-    month = list_u[1]
-    year = list_u[2]
-    if month == '07':
-        month = "July"
-    print(date, month, year + ' y.')
-
-
+    global index
+    digit_list = ['2', '3', '4', '5', '6', '7', '8', '9']
+    tel_num = ''
+    num_phone_number = ''
+    tel_num = input("Telephone number: ")
+    for ch in tel_num:
+        if ch.isalpha():
+            ch = ch.upper()
+            if ch == "A" or ch == "B" or ch == "C":
+                index = 0
+            elif ch == 'D' or ch == 'E' or ch == 'F':
+                index = 1
+            elif ch == 'G' or ch == 'H' or ch == 'I':
+                index = 2
+            elif ch == 'J' or ch == 'K' or ch == 'L':
+                index = 3
+            elif ch == 'M' or ch == 'N' or ch == 'O':
+                index = 4
+            elif ch == 'P' or ch == 'Q' or ch == 'R' or ch == 'S':
+                index = 5
+            elif ch == 'T' or ch == 'U' or ch == 'V':
+                index = 6
+            elif ch == 'W' or ch == 'X' or ch == 'Y' or ch == 'Z':
+                index = 7
+            ch = digit_list[index]
+        num_phone_number = num_phone_number + ch
+    print(num_phone_number)
 
 
 if __name__ == '__main__':
