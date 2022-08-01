5429b37202ef45363a53bfcf4708f9179805ef70
Anton <nevero.anton@gmail.com>
2022-08-01 19:10:36 +0300

    How many vowels and consonants

--- main.py
+++ main.py
@@ -2,28 +2,27 @@ import random
 
 
 def main():
-    answer = input("Enter the sentence: ")
-    answer_upper = to_upper(answer)
-    print(answer_upper)
+    answer = input("Enter the sentence: ").upper()
+    vowels = vowel_counter(answer)
+    consonants = consonant_counter(answer)
+    print(vowels, consonants)
+
+def vowel_counter(word):
+    count = 0
+    vowels = 'AEIOU'
+    for i in word:
+        if vowels.find(i) >= 0:
+            count += 1
+    return count
 
 
-def to_upper(word):
-    result = ''
-    new_sentence = True
-    result_word = ''
-
-    words = word.split()
-    for item in words:
-        if new_sentence:
-            result_word = item[0].upper() + item[1:]
-        else:
-            result_word = item
-        result = result + result_word + ' '
-        if item[-1] == '.' or item[-1] == '?' or item[-1] == '!':
-            new_sentence = True
-        else:
-            new_sentence = False
-    return result
+def consonant_counter(word):
+    consonants = "BCDFGJKLMNPQSTVXZHRWY"
+    count = 0
+    for i in word:
+        if consonants.find(i) >= 0:
+            count += 1
+    return count
 
 if __name__ == '__main__':
     main()
