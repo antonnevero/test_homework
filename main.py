710bbd0587a61f3e8ca120710733f098c7bc9301
Anton <nevero.anton@gmail.com>
2022-08-01 18:51:53 +0300

    To upper case

--- main.py
+++ main.py
@@ -2,29 +2,28 @@ import random
 
 
 def main():
-    upper_case = 0
-    lower_case = 0
-    isdigit = 0
-    isspace = 0
-    with open('text.txt', "r") as file:
-        file = file.readlines()
-    for i in file:
-        for j in i:
-            if j.isupper():
-                upper_case += 1
-            if j.islower():
-                lower_case += 1
-            if j.isdigit():
-                isdigit += 1
-            if j.isspace():
-                isspace += 1
-    print(upper_case)
-    print(lower_case)
-    print(isdigit)
-    print(isspace)
+    answer = input("Enter the sentence: ")
+    answer_upper = to_upper(answer)
+    print(answer_upper)
 
 
+def to_upper(word):
+    result = ''
+    new_sentence = True
+    result_word = ''
 
+    words = word.split()
+    for item in words:
+        if new_sentence:
+            result_word = item[0].upper() + item[1:]
+        else:
+            result_word = item
+        result = result + result_word + ' '
+        if item[-1] == '.' or item[-1] == '?' or item[-1] == '!':
+            new_sentence = True
+        else:
+            new_sentence = False
+    return result
 
 if __name__ == '__main__':
     main()
