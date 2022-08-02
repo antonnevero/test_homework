3bb0bcecd652329f83e6cc7ce99e4e6d75bf9e7d
Anton <nevero.anton@gmail.com>
2022-08-02 18:51:11 +0300

    Young slang

--- main.py
+++ main.py
@@ -2,17 +2,15 @@ import random
 
 
 def main():
-    answer = input("Enter the sentence: ")
-    result = answer[0]
+    answer = input("Enter the sentence: ").upper()
+    answer_list = answer.split()
+    final_list = []
 
-    for i in range(1, len(answer)):
-        ch = answer[i]
+    for i in answer_list:
+        final_list.append(i[1:] + i[0] + "КИ")
+    print(' '.join(final_list))
 
-        if ch.isupper():
-            ch = ch.lower()
-            result = result + " "
-        result = result + ch
-    print(result)
+
 
 if __name__ == '__main__':
     main()
