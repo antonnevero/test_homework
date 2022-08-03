ec1100204c6bec78aab60f246b7d2fe0c5373107
Anton <nevero.anton@gmail.com>
2022-08-03 19:06:28 +0300

    Capitals

--- main.py
+++ main.py
@@ -2,36 +2,29 @@ import random
 
 
 def main():
-    dict1 = {
-        'CS101': 3004,
-        'CS102': 4501,
-        'CS103': 6755,
-        'NT110': 1244,
-        'CM241': 1411
+    right = 0
+    wrong = 0
+    next = 0
+    dict = {
+        'Ukraine': "Kyiv",
+        'USA': "Washington",
+        'Australia': 'Canberra',
+        'Portugal': 'Lisbon',
+        'England': 'London'
     }
+    while next < 5:
+        country, capital = dict.popitem()
+        answer = input(f"What is the capital of {country}: ").title()
+        if answer == capital:
+            print('Right!')
+            right += 1
+        else:
+            print("Wrong!")
+            wrong += 1
+        next += 1
+        print(next)
 
-    dict2 = {
-        'CS101': 'Хайнс',
-        'CS102': 'Альварадо',
-        'CS103': 'Рич',
-        'NT110': 'Берк',
-        'CM241': 'Ли'
-    }
-
-    dict3 = {
-        'CS101': '08:00',
-        'CS102': '09:00',
-        'CS103': "10:00",
-        'NT110': '11:00',
-        'CM241': '13:00'
-    }
-    answer = input("Input number of course: ")
-    if answer not in dict1:
-        print("Wrong number of course")
-    else:
-        print(dict1[answer])
-        print(dict2[answer])
-        print(dict3[answer])
+    print(f"Right answers - {right}, wrong answers - {wrong}")
 
 
 if __name__ == '__main__':
