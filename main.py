7bcdf2495280f0c78b80437cf78419825f9d5c98
Anton <nevero.anton@gmail.com>
2022-08-03 19:35:50 +0300

    Unical words

--- main.py
+++ main.py
@@ -2,21 +2,13 @@ import random
 
 
 def main():
-    uncrypted_word = ''
-    codes = {
-        'a': '%',
-        'n': '!',
-        't': '$',
-        'o': '#'
-    }
+    word_set = set()
     with open('text.txt', 'r') as file:
-        text_list = file.readlines()
-    word = (''.join(text_list)).lower()
-    for i in word:
-        for k, v in codes.items():
-            if i == v:
-                uncrypted_word += k
-    print(uncrypted_word)
+        for w in file.readlines():
+            word_set.add(w.rstrip('\n'))
+
+    for i in word_set:
+        print(i)
 
 
 if __name__ == '__main__':
