b528ee0c9a2bb889366de03c73b933720488eb49
Anton <nevero.anton@gmail.com>
2022-08-05 18:53:54 +0300

    class Pet

--- main.py
+++ main.py
@@ -1,33 +1,41 @@
-class Book:
+class Pet:
 
-    def __init__(self, header, author, publisher):
-        self.__header = header
-        self.__author = author
-        self.__publisher = publisher
+    def __init__(self, name, animal_type, age):
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
 
-    def set_header(self, header):
-        self.__header = header
+    def set_name(self, name):
+        self.__name = name
 
-    def set_author(self, author):
-        self.__author = author
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
 
-    def set_publisher(self, publisher):
-        self.__publisher = publisher
+    def set_age(self, age):
+        self.__age = age
 
-    def get_header(self):
-        return self.__header
+    def get_name(self):
+        return self.__name
 
-    def get_author(self):
-        return self.__author
+    def get_animal_type(self):
+        return self.__animal_type
 
-    def get_publisher(self):
-        return self.__publisher
+    def get_age(self):
+        return self.__age
 
     def __str__(self):
         return self.__author, self.__header, self.__publisher
 
 
 def main():
+    name = input("Name: ")
+    animal_type = input("Animal_type: ")
+    age = input("age: ")
+    pet = Pet(name, animal_type, age)
+
+    print(pet.get_name())
+    print(pet.get_animal_type())
+    print(pet.get_age())
 
 
 if __name__ == '__main__':
