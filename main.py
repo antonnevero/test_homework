917c8032e73b2bd9f704839cdff7595f514da1dc
Anton <nevero.anton@gmail.com>
2022-08-08 18:58:20 +0300

    class RetailItem

--- main.py
+++ main.py
@@ -1,82 +1,127 @@
-class Patient:
-
-    def __init__(self, name, address, tel_number, emergency):
-        self.__name = name
-        self.__address = address
-        self.__tel_number = tel_number
-        self.__emergency = emergency
-
-    def get_name(self):
-        return self.__name
-
-    def get_address(self):
-        return self.__address
-
-    def get_tel_number(self):
-        return self.__tel_number
-
-    def get_emergency(self):
-        return self.__emergency
-
-    def set_name(self, name):
-        self.__name = name
-
-    def set_address(self, address):
-        self.__address = address
-
-    def set_tel_number(self, tel_number):
-        self.__tel_number = tel_number
-
-    def set_emergency(self, emergency):
-        self.__emergency = emergency
+PANTS = 1
+SHIRT = 2
+DRESS = 3
+SOCKS = 4
+SWEATER = 5
 
 
-class Procedure:
-
-    def __init__(self, name, date, doctor, price):
-        self.__name = name
-        self.__date = date
-        self.__doctor = doctor
+class RetailItem:
+    def __init__(self, description, inventory, price):
+        self.__description = description
+        self.__inventory = inventory
         self.__price = price
 
-    def get_name(self):
-        return self.__name
+    def set_description(self, description):
+        self.__description = description
 
-    def get_date(self):
-        return self.__date
-
-    def get_doctor(self):
-        return self.__doctor
-
-    def get_price(self):
-        return self.__price
-
-    def set_name(self, name):
-        self.__name = name
-
-    def set_date(self, date):
-        self.__date = date
-
-    def set_doctor(self, doctor):
-        self.__doctor = doctor
+    def set_inventory(self, inventory):
+        self.__inventory = inventory
 
     def set_price(self, price):
         self.__price = price
 
+    def get_description(self):
+        return self.__description
+
+    def get_inventory(self):
+        return self.__inventory
+
+    def get_price(self):
+        return self.__price
+
     def __str__(self):
-        return f"{self.get_name()}, {self.get_date()}, {self.get_doctor()}, {self.get_price()}"
+        result = 'Описание: ' + self.get_description() + '\n' + \
+                 'Единиц на складе: ' + str(self.get_inventory()) + \
+                 '\nЦена: $' + str(self.get_price())
+        return result
+
+
+class CashRegister:
+
+    def __init__(self):
+        self.__item_list = []
+
+    def purchase_item(self, item):
+        self.__item_list.append(item)
+
+    def get_total(self):
+        sum = 0
+        for i in self.__item_list:
+            sum += i.get_price()
+        return sum
+
+    def show_items(self):
+        for i in self.__item_list:
+            print(i)
+
+    def clear(self):
+        self.__item_list = []
 
 def main():
-    patient = Patient("Carl", "New York", 5555555555, "Jessica")
-    procedure1 = Procedure("osmotr", "08.08.2022", "Irvin", 250)
-    procedure2 = Procedure("rentgenoscopia", "08.08.2022", "Jameson", 500)
-    procedure3 = Procedure("krov", "08.08.2022", "Smith", 200)
 
-    print(patient.get_name(), patient.get_address(), patient.get_tel_number(), patient.get_emergency())
-    print(procedure1)
-    print(procedure2)
-    print(procedure3)
-    print(f"Sum is {procedure1.get_price() + procedure2.get_price() + procedure3.get_price()}")
+    # Создать продаваемые товары.
+    pants = RetailItem('Брюки', 10, 19.99)
+    shirt = RetailItem('Рубашка', 15, 12.50)
+    dress = RetailItem('Платье', 3, 79.00)
+    socks = RetailItem('Носки', 50, 1.00)
+    sweater = RetailItem('Свитер', 5, 49.99)
+
+    # Создать словарь продаваемых товаров.
+    sale_items = {PANTS: pants, SHIRT: shirt,
+                  DRESS: dress, SOCKS: socks,
+                  SWEATER: sweater}
+
+    # Создать кассовый аппарат.
+    register = CashRegister()
+
+    # Инициализировать проверку цикла.
+    checkout = 'Н'
+
+    # Пользователь хочет приобрести дополнительные товары:
+    while checkout == 'Н':
+
+        user_choice = get_user_choice()
+        item = sale_items[user_choice]
+        if item.get_inventory() == 0:
+            print('Этого товара нет в наличии.')
+        else:
+            register.purchase_item(item)
+
+            # Обновить товарную позицию
+            new_item = RetailItem(item.get_description(),
+                                         item.get_inventory() - 1,
+                                         item.get_price())
+            sale_items[user_choice] = new_item
+
+            checkout = input('Вы готовы оформить покупку (Д/Н)? ')
+
+    print()
+    print('Сумма Вашей покупки составляет: ',
+          format(register.get_total(), '.2f'))
+    print()
+    register.show_items()
+    register.clear()
+
+
+def get_user_choice():
+    print('Menu')
+    print('---------------------------------')
+    print('1. Брюки')
+    print('2. Рубашка')
+    print('3. Платье')
+    print('4. Носки')
+    print('5. Свитер')
+    print()
+
+    choice = int(input('Введите пункт меню товара, ' +
+                       'который вы хотели бы приобрести: '))
+    print()
+
+    while choice > SWEATER or choice < PANTS:
+        choice = int(input('Введите допустимый номер товара: '))
+        print()
+
+    return choice
 
 if __name__ == '__main__':
     main()
