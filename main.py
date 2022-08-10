9c4d1e5cd632d7d65c7678c8581d0c549d45210b
Anton <nevero.anton@gmail.com>
2022-08-10 19:40:29 +0300

    recursion 1.2

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 
 def main():
-    rec_print(5)
+    print(rec_print(5, 7))
 
 
-def rec_print(n):
-    if n > 1:
-        rec_print(n - 1)
-    print(n)
+def rec_print(x, y):
+    if x == 0 or y == 0:
+        return 0
+    else:
+        return x + rec_print(x, y - 1)
 
 
 if __name__ == '__main__':
