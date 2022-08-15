8a56552df026918726470746dfca6b54ce1303b0
Anton <nevero.anton@gmail.com>
2022-08-15 18:48:24 +0300

    recursion 1.4

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 
 def main():
-    print(rec_print(7))
+    print(rec_print([5, 7, 9, 3]))
 
 
 def rec_print(x):
-    if x > 1:
-        rec_print(x - 1)
-    for i in range(x):
-        print("*", end='')
-    print()
+    n = len(x)
+    if n == 1:
+        return x[0]
+    else:
+        temp = rec_print(x[0:n-1])
+        if x[n-1] > temp:
+            return x[n-1]
+        else:
+            return temp
+
+
 
 if __name__ == '__main__':
     main()
