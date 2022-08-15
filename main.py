6dab221a48d7515b928fa40f5adccf0e2881615b
Anton <nevero.anton@gmail.com>
2022-08-15 18:50:32 +0300

    recursion 1.5

--- main.py
+++ main.py
@@ -8,11 +8,7 @@ def rec_print(x):
     if n == 1:
         return x[0]
     else:
-        temp = rec_print(x[0:n-1])
-        if x[n-1] > temp:
-            return x[n-1]
-        else:
-            return temp
+        return x[n-1] + rec_print(x[0:n-1])
 
 
 
